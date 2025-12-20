c423f6bfde723c8f9dc30f8eabdebdf07f816cf6
mohmmed bssam <mohmmedbssam97@gmail.com>
2025-12-20 11:36:48 +0200

    complete to route

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+   class MainController extends Controller
+{
+
+public function home(){
+    return "home page";
+}
+public function about(){
+    return "about page";
+}
+public function contact(){
+    return "contact page";
+}
+
+}
+
+
--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,16 @@
 <?php
+
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-Route::get('/admin', function () {
-    return "Admin Dashboard";
-});
-Route::get('/admin/users', function () {
-    return "Admin Users List";
-});
+// Route::get('/admin', function () {
+//     return "Admin Dashboard";
+// });
+// Route::get('/admin/users', function () {
+//     return "Admin Users List";
+// });
+Route::get('/', [MainController::class, 'home'])->name('front.home');
+Route::get('/about', [MainController::class, 'about'])->name('front.about');
+Route::get('/contact', [MainController::class, 'contact'])->name('front.contact');
+
 
 ?>
